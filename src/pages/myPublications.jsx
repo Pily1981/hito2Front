72c8b123df2092671e91e5980b9480190abf9988
Tomas <tomascorvalan14@gmail.com>
2025-02-03 19:18:55 -0300

    demo

--- src/pages/myPublications.jsx
+++ src/pages/myPublications.jsx
@@ -7,26 +7,31 @@ import { faPen } from "@fortawesome/free-solid-svg-icons";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import { Container } from "react-bootstrap";
 
-
- const MyPublications = () => {
+const MyPublications = () => {
   return (
     <Container className="container-mp">
       <main className="publication-content-mp">
         <h2>Mis Publicaciones</h2>
         {productos.map((producto) => (
-        <div className="mp-inf" key={producto.id}>         
-          <h6>{producto.name}</h6>       
-          <h6>${producto.price}</h6>       
-        <NavLink to="/profile" className="fontawesome">
-          <span>  <FontAwesomeIcon icon={faPen} /></span> |
-          <span>  <FontAwesomeIcon icon={faTrash} /></span>
-        </NavLink>
-        </div>
-      ))}
+          <div className="mp-inf" key={producto.id}>
+            <h6>{producto.name}</h6>
+            <h6>${producto.price}</h6>
+            <NavLink to="/profile" className="fontawesome">
+              <span>
+                {" "}
+                <FontAwesomeIcon icon={faPen} />
+              </span>{" "}
+              |
+              <span>
+                {" "}
+                <FontAwesomeIcon icon={faTrash} />
+              </span>
+            </NavLink>
+          </div>
+        ))}
       </main>
     </Container>
   );
 };
 
 export default MyPublications;
-
