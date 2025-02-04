25ea5e1660bb34743edd44e4b45959b0cea3632b
Maria Pilar Jerez <mpjerezpalma@gmail.com>
2025-02-03 21:44:11 -0300

    se modifica vista mypublications

--- src/pages/myPublications.jsx
+++ src/pages/myPublications.jsx
@@ -7,27 +7,10 @@ import { faPen } from "@fortawesome/free-solid-svg-icons";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import { Container } from "react-bootstrap";
 
-
  const MyPublications = () => {
   return (
-    <div className="container-pf">
-      <aside className="profile-sidebar">
-        <ul className="menu-list">
-          <li className="menu-item" onClick={Profile}>
-            Datos Personales <FontAwesomeIcon icon={faChevronRight} />
-          </li>
-          <li className="menu-item" onClick={myPublications}>
-            Mis publicaciones <FontAwesomeIcon icon={faChevronRight} />
-          </li>
-          <li className="menu-item" onClick={createPublication}>
-            Crear publicación <FontAwesomeIcon icon={faChevronRight} />
-          </li>
-          <li className="menu-item" onClick={logout}>
-            Cerrar Sesión <FontAwesomeIcon icon={faChevronRight} />
-          </li>
-        </ul>
-      </aside>
-      <main className="publication-content-mp profile-content">
+   <Container className="container-mp">
+      <main className="publication-content-mp">
         <h2>Mis Publicaciones</h2>
         {productos.map((producto) => (
         <div className="mp-inf" key={producto.id}>         
@@ -40,7 +23,8 @@ import { Container } from "react-bootstrap";
         </div>
       ))}
       </main>
-    </div>
+      </Container>
+    
   );
 };
 
