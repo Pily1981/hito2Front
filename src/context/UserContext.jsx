36995b067f5fe0d7079270d2462239b3d960dccf
Paz <parancibiac@gmail.com>
2025-02-16 20:06:52 -0300

    agrego nuevamente user context

---
+++ src/context/UserContext.jsx
@@ -0,0 +1,37 @@
+import React, { createContext, useState, useContext } from "react";
+
+const UserContext = createContext();
+
+export const useUserContext = () => useContext(UserContext);
+
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(localStorage.getItem("token"));
+
+  const login = async (email, password) => {
+    try {
+      const response = await axios.post("http://localhost:3000/api/login", {
+        email,
+        password,
+      });
+      const { token } = response.data;
+      localStorage.setItem("token", token);
+      setToken(token);
+      setUser({ email });
+    } catch (error) {
+      throw new Error("Error en el login");
+    }
+  };
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    setToken(null);
+    setUser(null);
+  };
+
+  return (
+    <UserContext.Provider value={{ user, token, login, logout }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
